ffba4d5d4acbed01fe4454145e2a3bab091dc737
fymod <793064859@qq.com>
2018-11-18 18:32:26 +0800

    swagger2集成zuul和gateway

---
+++ 11 Swagger和Zuul、Gateway集成/demo-1/src/main/java/com/example/demo/MyController.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import java.util.Arrays;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+// Api:用在请求的类上，表示对类的说明
+// tags:说明该类的作用，可以在UI界面上看到的注解
+@Api(value="/test1", tags="这是模块名称1111111111")
+@RequestMapping("/test1")
+@RestController
+public class MyController {
+	// ApiOperation：用在请求的方法上，说明方法的用途、作用
+    // value:说明方法的用途、作用
+	// notes:方法的备注说明
+	@ApiOperation(value="这是列表", notes="这是列表")
+	@GetMapping("/list")
+	public List<String> getList() {
+		return Arrays.asList("str1", "str2", "str333", "str8888");
+	}
+	
+	@ApiOperation(value="添加用户信息", notes = "添加用户信息")
+    @ApiImplicitParam(name="user", value="User", required = true, dataType = "User")
+    @PostMapping("/addUser")
+    public User addUser(@RequestBody User user){
+		if(user.getId() < 10) {
+			return new User(0, "测试");
+		}
+        return new User(1, "有数据");
+    }
+	
+}
---
+++ 11 Swagger和Zuul、Gateway集成/demo/src/main/java/com/example/demo/DocumentationConfig.java
@@ -0,0 +1,45 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.cloud.netflix.zuul.filters.Route;
+import org.springframework.cloud.netflix.zuul.filters.RouteLocator;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+@Component
+@Primary
+public class DocumentationConfig implements SwaggerResourcesProvider{
+
+	private final RouteLocator routeLocator;
+
+    public DocumentationConfig(RouteLocator routeLocator) {
+        this.routeLocator = routeLocator;
+    }
+
+    @Override
+    public List<SwaggerResource> get() {
+        List<SwaggerResource> resources = new ArrayList<>();
+        List<Route> routes = routeLocator.getRoutes();
+        System.out.println(Arrays.toString(routes.toArray()));
+        routes.forEach(route -> {
+            resources.add(swaggerResource(route.getId(), route.getFullPath().replace("**", "v2/api-docs"),"2.0"));
+        });
+        return resources;
+    }
+
+    private SwaggerResource swaggerResource(String name, String location, String version) {
+        SwaggerResource swaggerResource = new SwaggerResource();
+        swaggerResource.setName(name);
+        swaggerResource.setLocation(location);
+        swaggerResource.setSwaggerVersion(version);
+        return swaggerResource;
+    }
+
+}
+
